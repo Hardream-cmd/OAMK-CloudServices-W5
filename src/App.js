72866bea96c689024808a6232c3b8dccc8167426
Arnaud <arnaud.a.Durand@gmail.com>
2022-10-01 12:52:12 +0300

    test continuous delivery

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My "Hello World!" WebApp
+          Testing the continuous delivery !
         </p>
       </header>
     </div>
